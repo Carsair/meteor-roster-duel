33eeda5f069df4e6bafe0fb3aa60952d86bf8e46
Carson Banov <carson@spanishdict.com>
2016-02-28 01:04:30 -0500

    Try to add some state.

--- client/players.js
+++ client/players.js
@@ -1,11 +1,19 @@
+// Get all the players to display them.
 Template.players.helpers({
   players: function () {
     return Players.find();
   }
 });
 
+// Click to add a player to your roster.
 Template.players.events({
   "click .player": function () {
-    Meteor.call('addToRoster', this)
+    // Gray out button in players list.
+    Players.update(this._id, {
+      $set: {checked: true}
+    });
+
+    // Add to roster.
+    Meteor.call('addToRoster', this);
   }
 });
--- client/roster.js
+++ client/roster.js
@@ -1,11 +1,20 @@
-Template.roster.events({
-  "click .player": function () {
-    Meteor.call('removeFromRoster', this)
-  }
-});
-
+// Get all the players to display them.
 Template.roster.helpers({
   players: function () {
     return Roster.find();
   }
 });
+
+// Click to remove a player from your roster.
+Template.roster.events({
+  "click .player": function () {
+    // Un-gray out button in players list.
+    Players.update(this._id, {
+      $set: {checked: false}
+    });
+
+    // Remove from roster.
+    Meteor.call('removeFromRoster', this)
+  }
+});
+
