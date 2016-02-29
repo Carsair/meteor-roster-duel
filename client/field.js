f8be493347f805db5de6af361b391d265c5f8810
Carson Banov <carson@spanishdict.com>
2016-02-29 13:08:12 -0500

    Allow adding players to field (need to have more checks in place like can't add player twice).

--- client/field.js
+++ client/field.js
@@ -9,9 +9,23 @@ Template.field.helpers({
   }
 });
 
-// Click to start adding a player to the field.
+// Click a player to start adding a player to the field.
 Template.field.events({
   "click .roster .player": function () {
     selected_player.set(this);
+  }
+});
+
+// Click a position to finalize adding a player to the field.
+Template.field.events({
+  "click .home_team .position": function (ev) {
+    player = selected_player.get();
+    if (player && player.name) {
+      $(ev.currentTarget).text(player.name);
+      selected_player.set();
+    }
+    else {
+      selected_player.set();
+    }
   }
 });
